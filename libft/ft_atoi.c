641ab7d37e383644de6e2b5da13492ea75045780
Ayoub Maatouch <aymaatou@e1r5p7.1337.ma>
2019-10-12 21:31:39 +0200

    Getting Started

---
+++ libft/ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aymaatou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/12 17:56:13 by aymaatou          #+#    #+#             */
+/*   Updated: 2019/10/12 19:54:58 by aymaatou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	number;
+	int	sign;
+
+	sign = 1;
+	i = 0;
+	number = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 48 && str[i] <= 57))
+			return (number * sign);
+		number = number * 10 + (str[i] - 48);
+		i++;
+	}
+	return (number * sign);
+}
