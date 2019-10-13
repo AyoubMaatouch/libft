2c081480ba9ebf9c9cf1b4e7cb1c4d70ab81f17a
Ayoub Maatouch <aymaatou@e1r5p7.1337.ma>
2019-10-13 15:59:26 +0200

    New Fix

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -1,25 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aymaatou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/12 22:48:51 by aymaatou          #+#    #+#             */
-/*   Updated: 2019/10/13 15:58:41 by aymaatou         ###   ########.fr       */
+/*   Created: 2019/10/13 15:43:34 by aymaatou          #+#    #+#             */
+/*   Updated: 2019/10/13 15:51:45 by aymaatou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
+#include <string.h>
 
-char	*ft_strchr(const char *str, int c)
+int	ft_strrchr(const char *str, int c)
 {
 	int i;
+	int j;
 
+	j = 0;
 	i = 0;
 	while (str[i] != c)
 	{
 		i++;
-		if (str[i] == '\0')
-			return (0);
+		if(str[i] == '\0')
+			return(0);
+		if (str[i] == c)
+			j = i;
 	}
-	return ((char *)&str[i]);
+	return ((char *)&str[j]);
 }
+
+int main () {
+	int len;
+	const char str[] = "http://www.tutorialspoint.com";
+	const char ch = '.';
+	char *ret;
+
+	ret = strrchr(str, ch);
+
+	printf("String after |%c| is - |%s|\n", ch, ret);
+	printf("String after |%c| is - |%s|\n", ch, ret);
+	return(0);
+}
+
