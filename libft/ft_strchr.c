caac0a362e0eb8f8dfd30aa6454ae88159d9af6e
Ayoub Maatouch <aymaatou@e1r5p7.1337.ma>
2019-10-13 13:10:56 +0200

    Fixing the code

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,50 +6,20 @@
 /*   By: aymaatou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 22:48:51 by aymaatou          #+#    #+#             */
-/*   Updated: 2019/10/13 12:54:29 by aymaatou         ###   ########.fr       */
+/*   Updated: 2019/10/13 13:08:30 by aymaatou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-int	ft_strlen(char *str)
+char	*ft_strchr(const char *str, int c)
 {
 	int i;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-char *ft_strchr(const char *str, int c)
-{
-	int i;
-	int j;
-	i = 0;
 	j = 0;
 	while (str[i] != c)
 	{
 		i++;
+		if (str[i] == '\0')
+			return (0);
 	}
-	if (str[i] == c)
-		return ((char *)&str[i]);
-	else
-		return (0);
-}
-int main () {
-	const char str[] = "http://www.tutorialspoint.com";
-	const char ch = 'z';
-	char *ret;
-	const char str1[] = "http://www.tutorialspoint.com";
-	const char ch1 = 'z';
-	char *ret1;
-	ret = strchr(str, ch);
-	ret1 = ft_strchr(str1, ch1);
-
-	printf("%c\n%s\n", ch, ret);
-	printf("%c\n%s\n", ch1, ret1);
-
-	return(0);
+	return ((char *)&str[i]);
 }
