370f81c95d19dc1038f8686bb889b1941646bbe6
Ayoub Maatouch <aymaatou@e1r5p7.1337.ma>
2019-10-13 21:19:02 +0200

    removing the main

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,11 +6,10 @@
 /*   By: aymaatou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 19:58:40 by aymaatou          #+#    #+#             */
-/*   Updated: 2019/10/13 20:18:32 by aymaatou         ###   ########.fr       */
+/*   Updated: 2019/10/13 21:18:32 by aymaatou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include<stdio.h>
-#include<string.h>
+
 char	*ft_strnstr(char *str, char *to_find, unsigned int n)
 {
 	int x;
@@ -18,7 +17,7 @@ char	*ft_strnstr(char *str, char *to_find, unsigned int n)
 
 	x = 0;
 	y = 0;
-	while ( x < n )
+	while (x < n)
 	{
 		y = 0;
 		while (str[x + y] == to_find[y])
@@ -29,17 +28,4 @@ char	*ft_strnstr(char *str, char *to_find, unsigned int n)
 		}
 		x++;
 	}
-	return (0);
-}
-
-int main () {
-	 char haystack[20] = "TutorialsPoint";
-	 char needle[10] = "u";
-	char *ret;
-
-	ret = strnstr(needle, haystack, 0);
-
-	printf("The substring is: %s\n", ret);
-
-	return(0);
 }
