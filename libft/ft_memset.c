1dfcc6bef5845592e4a6de254883df46a03ddab9
Ayoub Maatouch <aymaatou@e1r2p11.1337.ma>
2019-10-14 16:01:04 +0200

    adding ft_memset function

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,38 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aymaatou <aymaatou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/13 23:45:09 by aymaatou          #+#    #+#             */
-/*   Updated: 2019/10/14 14:08:00 by aymaatou         ###   ########.fr       */
+/*   Created: 2019/10/14 15:15:59 by aymaatou          #+#    #+#             */
+/*   Updated: 2019/10/14 15:59:20 by aymaatou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include <stdio.h>
-#include <string.h>
-
-int				ft_strlen(char *str);
-
-unsigned long	ft_strlcat(char *dst, const char *src, unsigned int size)
+void	*ft_memset(void *b, int c, unsigned int len)
 {
-	int		i;
-	char	*empty;
+	unsigned int i;
 
-	empty = dst;
-	i = ft_strlen((char *)src);
-	return (size + i);
-}
-
-int main () {
-	char src[] = "21345678";
-	char dest[] = "12345";
-	int i;
-	i = ft_strlcat(dest, src, 0);
-
-	printf("Final value : %d\n", i);
-
-	return(0);
+	i = 0;
+	while (len > i)
+	{
+		((char*)b)[i] = c;
+		i++;
+	}
+	return ;
 }
