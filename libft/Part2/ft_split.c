4dacae5a9ac162621e086de372bd788c25b02943
Ayoub Maatouch <aymaatou@e2r10p5.1337.ma>
2019-10-22 19:51:23 +0200

    ft_splite 1.0

--- libft/Part2/ft_split.c
+++ libft/Part2/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: aymaatou <aymaatou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 18:02:04 by aymaatou          #+#    #+#             */
-/*   Updated: 2019/10/22 18:58:02 by aymaatou         ###   ########.fr       */
+/*   Updated: 2019/10/22 19:24:00 by aymaatou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,30 +53,36 @@ static void		free_ptr(char **ptr, int j)
 	free(ptr);
 }
 
+static int		str_len(char *occ_ptr, char c)
+{
+	int i;
+
+	i = 0;
+	while (occ_ptr[i] != 0)
+	{
+		if (occ_ptr[i] == c && occ_ptr[i + 1] != c)
+			break ;
+		i++;
+	}
+	return (i);
+}
+
 char			**ft_split(char const *s, char c)
 {
-	char	**g_ptr;
-	char	*occ_ptr;
-	int		i;
-	int		nb_occ;
-	int		j;
+	char			**g_ptr;
+	char			*occ_ptr;
+	int				i;
+	int				nb_occ;
+	static int		j;
 
 	while (*s == c)
 		s++;
 	occ_ptr = (char*)s;
 	nb_occ = nb(occ_ptr, c);
 	g_ptr = (char **)malloc(nb_occ * sizeof(char *) + 1);
-	j = 0;
-	occ_ptr = (char*)s;
 	while (j < nb_occ)
 	{
-		i = 0;
-		while (occ_ptr[i] != 0)
-		{
-			if (occ_ptr[i] == c && occ_ptr[i + 1] != c)
-				break ;
-			i++;
-		}
+		i = str_len(occ_ptr, c);
 		if (!(g_ptr[j] = (char *)malloc(sizeof(char) * i)))
 		{
 			free_ptr(g_ptr, j - 1);
@@ -84,24 +90,8 @@ char			**ft_split(char const *s, char c)
 		}
 		dp_print(g_ptr[j], occ_ptr, c, i);
 		occ_ptr += i + 1;
-		
-	puts (g_ptr[j]);
 		j++;
 	}
 	g_ptr[j] = NULL;
 	return (g_ptr);
-}
-int main()
-{
-	char **str;
-   	str =ft_split("  hello am heere    yes     i am   ", ' ');
-	int i = 0;
-	while (str[i] != NULL)
-	{
-		puts(str[i]);
-		i++;
-		}
-
-		return (0);
-	
 }
